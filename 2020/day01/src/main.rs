6319f0e679d482f0a974cd628c4e646617c28f51
J2ghz <j2.00ghz@gmail.com>
2020-12-01 22:44:18 +0100

    Day 1 2020

---
+++ 2020/day01/src/main.rs
@@ -0,0 +1,43 @@
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
+
+fn main2() {
+    let lines = read_lines("input.txt").unwrap();
+    let mut old_numbers = Vec::new();
+    for line in lines {
+        let number = line.unwrap().parse::<i32>().unwrap();
+        for old in old_numbers.iter() {
+            if number + old == 2020 {
+                println!("{}x{}={}", number, old, number * old);
+            }
+        }
+        old_numbers.insert(0, number);
+    }
+}
+
+fn main() {
+    let lines = read_lines("input.txt").unwrap();
+    let mut old_numbers = Vec::new();
+    for line in lines {
+        let number = line.unwrap().parse::<i32>().unwrap();
+        for old2 in old_numbers.iter() {
+            for old in old_numbers.iter() {
+                if number + old +old2 == 2020 {
+                    println!("{}x{}z{}={}", number, old,old2, number * old*old2);
+                }
+            }
+        }
+        old_numbers.insert(0, number);
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
