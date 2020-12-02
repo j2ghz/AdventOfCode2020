8c1b83501c98113aaa0bc788e27a8c477ebb5968
J2ghz <j2.00ghz@gmail.com>
2020-12-02 23:43:12 +0100

    day 2 part 1

---
+++ 2020/day02/src/main.rs
@@ -0,0 +1,37 @@
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
+
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
+    let lines = read_lines("input.txt").unwrap();
+    let mut allCount = 0;
+    for line in lines {
+        let line = line.unwrap();
+        println!("{}",line);
+        let c = re.captures(&line).unwrap();
+        println!("{:?}",c);
+        let min: usize = c.get(1).unwrap().as_str().parse().unwrap();
+        let max: usize = c.get(2).unwrap().as_str().parse().unwrap();
+        let p = c.get(3).unwrap().as_str().chars().next().unwrap();
+        let passw = c.get(4).unwrap().as_str();
+        let count = passw.chars().into_iter().filter(|c| *c == p).count();
+        println!("{} has {}x'{}'", passw, count, p);
+        if count >= min && count <= max {
+            allCount += 1;
+        }
+    }
+    println!("Valid: {}", allCount);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
