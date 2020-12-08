6b67cd632ff3ca800e104ae2acd197cf0c368071
J2ghz <j2.00ghz@gmail.com>
2020-12-08 18:34:59 +0100

    try aoc crate

---
+++ src/day8.rs
@@ -0,0 +1,23 @@
+pub fn generator(input: &str) -> Vec<u64> {
+    input
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect()
+}
+
+pub fn part_1(input: &[u64]) -> u64 {
+    input.iter().map(|&mass| mass / 3 - 2).sum()
+}
+
+pub fn part_2(input: &[u64]) -> u64 {
+    fn total_needed_mass(obj: u64) -> u64 {
+        if obj < 9 {
+            0
+        } else {
+            let obj_mass = obj / 3 - 2;
+            obj_mass + total_needed_mass(obj_mass)
+        }
+    }
+
+    input.iter().copied().map(total_needed_mass).sum()
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod day8;
+
+aoc::main! {
+    year 2020;
+    day8 : generator => part_1, part_2;
+}
