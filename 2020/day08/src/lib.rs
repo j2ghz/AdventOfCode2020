e964e399a3ea56369bf0b35ca312e361320ff4b1
J2ghz <j2.00ghz@gmail.com>
2020-12-10 22:37:24 +0100

    part2

--- 2020/day08/src/lib.rs
+++ 2020/day08/src/lib.rs
@@ -13,8 +13,9 @@ use nom::{
     IResult,
 };
 
+#[derive(Clone)]
 enum Instruction {
-    Nop,
+    Nop(i32),
     Acc(i32),
     Jmp(i32),
 }
@@ -25,7 +26,7 @@ fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
         let num = num.parse::<i32>().unwrap();
         let num = if sign == '-' { -num } else { num };
         match instr {
-            "nop" => Instruction::Nop,
+            "nop" => Instruction::Nop(num),
             "acc" => Instruction::Acc(num),
             "jmp" => Instruction::Jmp(num),
             _ => panic!("Unknown instruction {}", instr),
@@ -41,7 +42,7 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn stop_on_loop(file: &str) -> i32 {
+pub fn stop_on_loop(file: &str) -> i32 {
     let mut visited = HashSet::new();
     let instructions: HashMap<usize, Instruction> = read_lines(file)
         .unwrap()
@@ -54,7 +55,7 @@ fn stop_on_loop(file: &str) -> i32 {
     while !visited.contains(&index) {
         visited.insert(index);
         match instructions.get(&index).unwrap() {
-            Instruction::Nop => index += 1,
+            Instruction::Nop(_) => index += 1,
             Instruction::Acc(i) => {
                 accumulator += i;
                 index += 1
@@ -71,9 +72,69 @@ fn stop_on_loop(file: &str) -> i32 {
     accumulator
 }
 
+fn halts(instructions: &HashMap<usize, Instruction>) -> Option<i32> {
+    let mut visited = HashSet::new();
+    let mut accumulator = 0;
+    let mut index = 0;
+    while !visited.contains(&index) {
+        visited.insert(index);
+        match instructions.get(&index).unwrap() {
+            Instruction::Nop(_) => index += 1,
+            Instruction::Acc(i) => {
+                accumulator += i;
+                index += 1
+            }
+            Instruction::Jmp(i) => {
+                index = if i.is_negative() {
+                    index.checked_sub((-i) as usize).unwrap()
+                } else {
+                    index.checked_add(*i as usize).unwrap()
+                }
+            }
+        }
+        if index == instructions.len() {
+            return Some(accumulator);
+        }
+    }
+    None
+}
+
+pub fn try_until_halts(file: &str) -> i32 {
+    let instructions: HashMap<usize, Instruction> = read_lines(file)
+        .unwrap()
+        .map(Result::unwrap)
+        .map(|l| parse_instruction(&l).unwrap().1)
+        .enumerate()
+        .collect();
+
+    for i in 0..instructions.len() {
+        let mut changed_instructions = instructions.clone();
+        changed_instructions
+            .entry(i)
+            .and_modify(|instr| match instr {
+                Instruction::Nop(i) => *instr = Instruction::Jmp(*i),
+                Instruction::Jmp(i) => *instr = Instruction::Nop(*i),
+                _ => (),
+            });
+        if let Some(acc) = halts(&changed_instructions) {
+            return acc;
+        }
+    }
+    panic!("Never halts")
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::stop_on_loop;
+    use crate::{stop_on_loop, try_until_halts};
+
+    #[test]
+    fn part_2_example() {
+        assert_eq!(8, try_until_halts("example.txt"));
+    }
+    #[test]
+    fn part_2() {
+        assert_eq!(8, try_until_halts("input.txt"));
+    }
 
     #[test]
     fn part_1_example() {
@@ -83,5 +144,4 @@ mod tests {
     fn part_1() {
         assert_eq!(2025, stop_on_loop("input.txt"));
     }
-
 }
