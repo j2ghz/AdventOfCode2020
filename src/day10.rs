1246fea9f2ebf995a5fb4a53f3c53a4f531b7da0
J2ghz <j2.00ghz@gmail.com>
2020-12-12 15:29:05 +0100

    use cargo-aoc

---
+++ src/day10.rs
@@ -0,0 +1,42 @@
+use itertools::Itertools;
+
+#[aoc_generator(day10)]
+pub fn input_generator(input: &str) -> Vec<u32> {
+    input
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .collect::<Vec<u32>>()
+}
+#[aoc(day10, part1)]
+pub fn solve_part1(input: &[u32]) -> u32 {
+    let (d1, d3) = input
+        .iter()
+        .sorted()
+        .tuple_windows()
+        .fold((0, 0), |(d1, d3), (p, n)| match n - p {
+            1 => (d1 + 1, d3),
+            3 => (d1, d3 + 1),
+            _ => (d1, d3),
+        });
+    (d1 + 1) * (d3 + 1)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve_part1;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(35, solve_part1(&[16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]));
+    }
+    #[test]
+    fn part1_example2() {
+        assert_eq!(
+            220,
+            solve_part1(&[
+                28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25,
+                35, 8, 17, 7, 9, 4, 2, 34, 10, 3
+            ])
+        );
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+use aoc_runner_derive::aoc_lib;
+
+mod day10;
+
+aoc_lib! { year = 2020 }
