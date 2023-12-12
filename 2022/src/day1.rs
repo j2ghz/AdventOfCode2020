d22d3eef9aff56bd5828205052a5e2f07c743413
Jozef Hollý <1708197+j2ghz@users.noreply.github.com>
2023-12-12 19:45:46 +0100

    fix compilation

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -4,26 +4,25 @@ use color_eyre::Result;
 use itertools::Itertools;
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<Vec<u32>> {
+pub fn input_generator(input: &str) -> Vec<Vec<Option<u32>>> {
     input
         .split("\n\n")
-        .map(|g| g.lines().map(|l| l.parse::<u32>().unwrap()).collect())
+        .map(|g| {
+            g.lines()
+                .map(|l| l.parse::<u32>().map_or(None, |i| Some(i)))
+                .collect()
+        })
         .collect()
 }
 
 #[aoc(day1, part1)]
-pub fn part1(input: &[Vec<u32>]) -> u32 {
-    input.iter().map(|g| g.iter().sum()).max().unwrap()
+pub fn part1(input: &[Vec<Option<u32>>]) -> u32 {
+    0
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &[Vec<u32>]) -> u32 {
-    input
-        .iter()
-        .map(|g| g.iter().sum::<u32>())
-        .sorted_by_key(|x| Reverse(*x))
-        .take(3)
-        .sum()
+pub fn part2(input: &[Vec<Option<u32>>]) -> u32 {
+    0
 }
 
 #[cfg(test)]
@@ -32,19 +31,13 @@ mod tests {
 
     #[test]
     fn part1() {
-        let input = read_to_string("input/2020/day12.txt").expect("input file missing");
-        assert_eq!(
-            0,
-            super::part1(&super::input_generator(&input).unwrap()).unwrap()
-        );
+        let input = read_to_string("input/2022/day1.txt").expect("input file missing");
+        assert_eq!(0, super::part1(&super::input_generator(&input)));
     }
 
     #[test]
     fn part2() {
-        let input = read_to_string("input/2020/day12.txt").expect("input file missing");
-        assert_eq!(
-            0,
-            super::part2(&super::input_generator(&input).unwrap()).unwrap()
-        );
+        let input = read_to_string("input/2022/day1.txt").expect("input file missing");
+        assert_eq!(0, super::part2(&super::input_generator(&input)));
     }
 }
